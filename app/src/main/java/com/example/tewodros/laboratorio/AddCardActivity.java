68e9cac8db06a48ffa31eb1b5c87c425704e4fde
Tewodros Amare <teddygizachew1@gmail.com>
2020-03-21 16:39:53 -0400

    Lab 3

--- app/src/main/java/com/example/tewodros/laboratorio/AddCardActivity.java
+++ app/src/main/java/com/example/tewodros/laboratorio/AddCardActivity.java
@@ -3,9 +3,13 @@ package com.example.tewodros.laboratorio;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -13,6 +17,16 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        String s1 = getIntent().getStringExtra("edit1");
+        String s2 = getIntent().getStringExtra("edit2");
+        String s3 = getIntent().getStringExtra("edit3");
+        String s4 = getIntent().getStringExtra("edit4");
+
+        ((TextView) findViewById(R.id.new_question)).setText(s1);
+        ((TextView) findViewById(R.id.new_answer1)).setText(s2);
+        ((TextView) findViewById(R.id.new_answer2)).setText(s3);
+        ((TextView) findViewById(R.id.new_answer3)).setText(s4);
 
         findViewById(R.id.cancelBtn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,13 +38,23 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.saveBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String questions = ((EditText) findViewById(R.id.Question)).getText().toString();
-                String answers = ((EditText) findViewById(R.id.Answer)).getText().toString();
+                String q1 = ((EditText) findViewById(R.id.new_question)).getText().toString();
+                String a1 = ((EditText) findViewById(R.id.new_answer1)).getText().toString();
+                String a2 = ((EditText) findViewById(R.id.new_answer2)).getText().toString();
+                String a3 = ((EditText) findViewById(R.id.new_answer3)).getText().toString();
                 Intent data = new Intent();
-                data.putExtra("string1", questions);
-                data.putExtra("string2", answers);
+                data.putExtra("string1", q1);
+                data.putExtra("string2", a1);
+                data.putExtra("string3", a2);
+                data.putExtra("string4", a3);
                 setResult(RESULT_OK, data);
-                finish();
+                if (q1.isEmpty() || a1.isEmpty() || a2.isEmpty() || a3.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Must enter all fields!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    finish();
+                }
+                overridePendingTransition(R.anim.left_in, R.anim.left_out);
             }
         });
     }
--- app/src/main/java/com/example/tewodros/laboratorio/MainActivity.java
+++ app/src/main/java/com/example/tewodros/laboratorio/MainActivity.java
@@ -7,13 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isShowingAnswers = false;
     FlashcardDatabase flashcardDatabase;
     List<Flashcard> allFlashcards;
     int currentCardDisplayedIndex = 0;
@@ -29,25 +33,34 @@ public class MainActivity extends AppCompatActivity {
         if (allFlashcards != null && allFlashcards.size() > 0) {
             ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
             ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(0).getAnswer());
+            ((TextView) findViewById(R.id.flashcard_answer2)).setText(allFlashcards.get(0).getWrongAnswer1());
+            ((TextView) findViewById(R.id.flashcard_answer3)).setText(allFlashcards.get(0).getWrongAnswer2());
         }
-
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
-                /*
-                (findViewById(R.id.flashcard_question)).setBackgroundColor(
-                        getResources().getColor(R.color.my_red_color, null));
-                */
-            }
-        });
 
         findViewById(R.id.flashcard_answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+                findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.green, null));
+            }
+        });
+
+        findViewById(R.id.flashcard_answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
+                findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+                findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.green, null));
+            }
+        });
+
+        findViewById(R.id.flashcard_answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.green, null));
+                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+                findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.my_original, null));
             }
         });
 
@@ -56,7 +69,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
-                }
+                overridePendingTransition(R.anim.right_in, R.anim.right_out);
+            }
         });
 
         findViewById(R.id.nextBtn).setOnClickListener(new View.OnClickListener() {
@@ -69,22 +83,117 @@ public class MainActivity extends AppCompatActivity {
                     currentCardDisplayedIndex = 0;
                 }
                 // set the question and answer TextViews with data from the database
-                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
-                ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                if (allFlashcards.size() != 0) {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer1)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    ((TextView) findViewById(R.id.flashcard_answer2)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer1());
+                    ((TextView) findViewById(R.id.flashcard_answer3)).setText(allFlashcards.get(currentCardDisplayedIndex).getWrongAnswer2());
+                }
+
+//                ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.invisible);
+//                findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+//                findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+//                findViewById(R.id.flashcard_answer3).setVisibility(View.VISIBLE);
+//                //isShowingAnswers = false;
+//                findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+//                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+//                findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.my_original, null));
             }
         });
+
+        findViewById(R.id.resetBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+                findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+            }
+        });
+
+        findViewById(R.id.edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                String value1 = ((TextView) findViewById(R.id.flashcard_question)).getText().toString();
+                String value2 = ((TextView) findViewById(R.id.flashcard_answer1)).getText().toString();
+                String value3 = ((TextView) findViewById(R.id.flashcard_answer2)).getText().toString();
+                String value4 = ((TextView) findViewById(R.id.flashcard_answer3)).getText().toString();
+                intent.putExtra("edit1", value1);
+                intent.putExtra("edit2", value2);
+                intent.putExtra("edit3", value3);
+                intent.putExtra("edit4", value4);
+                MainActivity.this.startActivityForResult(intent, 101);
+            }
+        });
+
+//        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (!isShowingAnswers) {
+//                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.invisible);
+//                    findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+//                    findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+//                    findViewById(R.id.flashcard_answer3).setVisibility(View.VISIBLE);
+//                    isShowingAnswers = true;
+//                }
+//                else if (isShowingAnswers) {
+//                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.visible);
+//                    findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+//                    findViewById(R.id.flashcard_answer2).setVisibility(View.INVISIBLE);
+//                    findViewById(R.id.flashcard_answer3).setVisibility(View.INVISIBLE);
+//                    isShowingAnswers = false;
+//                }
+//            }
+//        });
+
+//        findViewById(R.id.deleteBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+//            }
+//        });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (resultCode == RESULT_OK && requestCode == 100) {
             String question = data.getStringExtra("string1");
-            String answer = data.getStringExtra("string2");
+            String answer1 = data.getStringExtra("string2");
+            String answer2 = data.getStringExtra("string3");
+            String answer3 = data.getStringExtra("string4");
             ((TextView)findViewById(R.id.flashcard_question)).setText(question);
-            ((TextView)findViewById(R.id.flashcard_answer1)).setText(answer);
-            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            ((TextView)findViewById(R.id.flashcard_answer1)).setText(answer1);
+            ((TextView)findViewById(R.id.flashcard_answer2)).setText(answer2);
+            ((TextView)findViewById(R.id.flashcard_answer3)).setText(answer3);
+            flashcardDatabase.insertCard(new Flashcard(question, answer1, answer2, answer3));
             allFlashcards = flashcardDatabase.getAllCards();
+            Snackbar.make(findViewById(R.id.flashcard_question), "Card successfully created!", Snackbar.LENGTH_SHORT).show();
         }
+        if (resultCode == RESULT_OK && requestCode == 101) {
+            String question = data.getStringExtra("string1");
+            String answer1 = data.getStringExtra("string2");
+            String answer2 = data.getStringExtra("string3");
+            String answer3 = data.getStringExtra("string4");
+            ((TextView)findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView)findViewById(R.id.flashcard_answer1)).setText(answer1);
+            ((TextView)findViewById(R.id.flashcard_answer2)).setText(answer2);
+            ((TextView)findViewById(R.id.flashcard_answer3)).setText(answer3);
+            flashcardDatabase.updateCard(new Flashcard(question, answer1, answer2, answer3));
+            allFlashcards = flashcardDatabase.getAllCards();
+            Snackbar.make(findViewById(R.id.flashcard_question), "Card successfully edited!", Snackbar.LENGTH_SHORT).show();
+        }
+
+//        findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+//        findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+//        findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.my_original, null));
+//        findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+//        findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+//        findViewById(R.id.flashcard_answer3).setVisibility(View.VISIBLE);
+
     }
 }
